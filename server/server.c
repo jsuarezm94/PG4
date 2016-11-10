925cb6214a61bebf60da1605141c0b0c1ddfa5df
Jorge Diaz-Ortiz <jdiazort@nd.edu>
2016-11-10 18:34:12 -0500

    first server edit

--- server/server.c
+++ server/server.c
@@ -28,6 +28,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <fcntl.h>
+#include <string.h>
+
 #define MAX_MD5LENGTH 50
 #define MAX_FILENAME 100
 #define MAX_PENDING 5
@@ -39,74 +42,327 @@ struct user {
 	char password[100];
 };
 
+int32_t fileSize(char * file_name) {
 
+       	FILE *fp;
+        int32_t file_size = -1;
+        if(fp = fopen(file_name, "r")) {
+                fseek(fp,0,SEEK_END);
+                file_size = ftell(fp);
+                fseek(fp,0,SEEK_SET);
+                fclose(fp);
+        }
+        return file_size;
+
+} //end FILESIZE
+
+
+
+
+/* CREATE BOARD */
+void create(int udp_sock, struct  sockaddr_in sin, char * user_name) {
+
+	printf("inside CREATE\n");
+		
+	/* Declare Variables  */
+	char board_name[100];
+	char server_resp[100];
+	char board_head[200];	
+
+	int fp;
+
+	int addr_len;
+
+	memset(board_name, '\0', sizeof(board_name));
+        memset(server_resp, '\0', sizeof(server_resp));
+        memset(board_head, '\0', sizeof(board_head));
+
+	
+        /* Receive board_name of board to create from the client */
+        if(recvfrom(udp_sock,board_name,strlen(board_name),0,(struct sockaddr *)&sin, &addr_len) == -1) {
+                perror("ERROR: client-recvfrom()\n");
+                exit(1);
+        } //end recvfrom check
+
+	fp = open(board_name, O_CREAT, S_IRUSR | S_IWUSR);
+
+	strcpy(board_head, user_name);
+	strcat(board_head, " ");
+	strcat(board_head, board_name);
+	strcat(board_head, "\n");
+
+	int wf = write(fp, board_head, sizeof(board_head) );
+	printf("Number of bytes written = %i\n");
+
+
+	strcpy(server_resp, "Yes" );
+
+	if (sendto (udp_sock, server_resp, strlen(server_resp), 0, (struct sockaddr *)&sin, sizeof(struct sockaddr) ) == -1 ) {
+                perror("ERROR: server-sendto()\n");
+
+	}
+
+	printf("End of create\n");
+
+
+}
+
+
+void message(int udp_sock, struct  sockaddr_in sin, char * user_name) {
+
+	/* Declare cariables */
+	char board_name[100];
+	char message[4096];
+	char server_resp[100];
+        int addr_len;
+	
+
+        memset(board_name, '\0', sizeof(board_name));
+        memset(message, '\0', sizeof(message));
+        memset(server_resp, '\0', sizeof(server_resp));
+
+        /* Receive board_name of board to create from the client */
+        if(recvfrom(udp_sock,board_name,strlen(board_name),0,(struct sockaddr *)&sin, &addr_len) == -1) {
+                perror("ERROR: client-recvfrom()\n");
+                exit(1);
+        } //end recvfrom check
+
+        /* Receive message to post on board */
+        if(recvfrom(udp_sock,message,strlen(message),0,(struct sockaddr *)&sin, &addr_len) == -1) {
+                perror("ERROR: client-recvfrom()\n");
+                exit(1);
+        } //end recvfrom check
+
+	/*
+ 	bool found = false;
+	for( size_t i=0; i<num_boards; i++) {
+		if (boards[i] == board_name) {
+			
+		}
+	}  
+
+	if (found) {
+		post message
+		if success
+			strcpy(server_resp, "Yes");
+		else 
+			strpy(server_resp, "No");
+	}
+	else {
+		strpcy(server_resp, "No);
+	}
+	
+	*/
+	
+	strcpy(server_resp,"No");
+
+	if(sendto(udp_sock,board_name,strlen(board_name),0,(struct sockaddr *)&sin, sizeof(struct sockaddr)) == -1) {
+                perror("ERROR: client-sendto()\n");
+                exit(1);
+        } //end sendto check
+
+}
+
+
+void delete (int udp_sock, struct  sockaddr_in sin, char * user_name) {
+	
+	/* Declare Variables */
+	char board_name[100];		
+	char message[4096];
+        char server_resp[100];
+        int addr_len;
+
+
+        memset(board_name, '\0', sizeof(board_name));
+        memset(message, '\0', sizeof(message));
+        memset(server_resp, '\0', sizeof(server_resp));
+
+	/* Receive board_name of boad to delete */
+        if(recvfrom(udp_sock,board_name,strlen(board_name),0,(struct sockaddr *)&sin, &addr_len) == -1) {
+                perror("ERROR: client-recvfrom()\n");
+                exit(1);
+        } //end recvfrom check
+	
+	/* Receive message to be deleted */
+        if(recvfrom(udp_sock,message,strlen(message),0,(struct sockaddr *)&sin, &addr_len) == -1) {
+                perror("ERROR: client-recvfrom()\n");
+                exit(1);
+        } //end recvfrom check
+	
+	/* PERFORM DELETION*/
+
+
+
+
+	/* END DELETION */	
+
+        strcpy(server_resp,"No");
+        if(sendto(udp_sock,board_name,strlen(board_name),0,(struct sockaddr *)&sin, sizeof(struct sockaddr)) == -1) {
+                perror("ERROR: client-sendto()\n");
+                exit(1);
+        } //end sendto check
+
+
+}
+
+
+
+void edit (int udp_sock, struct  sockaddr_in sin, char * user_name) {
+
+        /* Declare Variables */
+        char board_name[100];
+        char message[4096];
+	char new_message[4096];
+        char server_resp[100];
+        int addr_len;
+
+
+        memset(board_name, '\0', sizeof(board_name));
+        memset(message, '\0', sizeof(message));
+        memset(server_resp, '\0', sizeof(server_resp));
+
+        /* Receive board_name of boad to delete */
+        if(recvfrom(udp_sock,board_name,strlen(board_name),0,(struct sockaddr *)&sin, &addr_len) == -1) {
+                perror("ERROR: client-recvfrom()\n");
+                exit(1);
+        } //end recvfrom check
+
+        /* Receive message to be edited */
+        if(recvfrom(udp_sock,message,strlen(message),0,(struct sockaddr *)&sin, &addr_len) == -1) {
+                perror("ERROR: client-recvfrom()\n");
+                exit(1);
+        } //end recvfrom check
+
+	/* Receive edited message*/
+        if(recvfrom(udp_sock,new_message,strlen(new_message),0,(struct sockaddr *)&sin, &addr_len) == -1) {
+                perror("ERROR: client-recvfrom()\n");
+                exit(1);
+	}
+
+        /* PERFORM REPLACEMENT */
+
+
+
+
+        /* END DELETION */
+
+        strcpy(server_resp,"No");
+        if(sendto(udp_sock,server_resp,strlen(server_resp),0,(struct sockaddr *)&sin, sizeof(struct sockaddr)) == -1) {
+                perror("ERROR: client-sendto()\n");
+                exit(1);
+        } //end sendto check
+
+
+}
+
+
+void list (int udp_sock, struct  sockaddr_in sin, char * user_name) {
+	
+        /* Declare Variables */
+	char server_response[100];
+	char board_names[4096];
+        int addr_len;
+
+
+        memset(board_names, '\0', sizeof(board_names));
+
+	/* GET LIST OF ALL THE BOARDS*/
+
+
+
+	/* */
+
+	if(sendto(udp_sock,board_names,strlen(board_names),0,(struct sockaddr *)&sin, sizeof(struct sockaddr)) == -1) {
+                perror("ERROR: client-sendto()\n");
+                exit(1);
+        } //end sendto chec
+}
+
+/*
+void readBoard(int udp_sock, struct  so) {
+
+        char board_name[100];
+        char message[4096];
+        char server_resp[100];
+
+        memset(board_name, '\0', sizeof(board_name));
+        memset(message, '\0', sizeof(message));
+        memset(server_resp, '\0', sizeof(server_resp));
+
+}
+*/
 
 
 int main (int argc, char * argv[]) {
 
-   /* Declare variables */
-   struct       sockaddr_in sin;        // Address structure
-   int          addr_len;               // Length
-   char         buf[10];                // Buffer for commands
-//   int          sock, new_sock;         // Sockets
-   int          opt = 1;
-   int          port_num;               // Port number
+	/* Declare variables */
+	struct       sockaddr_in sin;        // Address structure
+	int          addr_len;               // Length
 
+        int     udp_sock;               // UDP - File handle for socket
+        int     tcp_sock;               // TCP - File handle for socket
+
+	char         buf[10];                // Buffer for commands
+	//   int          sock, new_sock;         // Sockets
+	int          opt = 1;
+	int          port_num;               // Port number
 
 	struct user users[100];
 	int total_users = 0;
 
+        int     exit_loop=1;            // While loop variable
+        int     shutdown_ind;           // Shutdown indicator
 
-   /* Check arguments in command line */
-   if (argc!=2) {
-      fprintf(stderr,"USAGE ERROR: %s <port number>\n",argv[0]);
-      exit(1);
-   } // end arguments check
 
-   /* Assign command-line arguments and check for errors*/
-   /* Port number - argv1 */
-   port_num = atoi(argv[1]);
 
-   /* Build address data structure */
-   bzero((char *)&sin, sizeof(sin));
-   sin.sin_family = AF_INET;
+
+   	/* Check arguments in command line */
+   	if (argc!=2) {
+      		fprintf(stderr,"USAGE ERROR: %s <port number>\n",argv[0]);
+      		exit(1);
+   	} // end arguments check
+
+	/* Assign command-line arguments and check for errors*/
+   	/* Port number - argv1 */
+  	port_num = atoi(argv[1]);
+
+   	/* Build address data structure */
+   	bzero((char *)&sin, sizeof(sin));
+   	sin.sin_family = AF_INET;
         printf("HERE 1\n");
-   sin.sin_addr.s_addr = INADDR_ANY;
-   sin.sin_port = htons(port_num);
-
+   	sin.sin_addr.s_addr = INADDR_ANY;
+   	sin.sin_port = htons(port_num);
 
 	/* --- Setting up the UDP server --- */
-
-
+        if((udp_sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+                perror("ERROR: client-socket()\n");
+                exit(1);
+        } // end socket check
 
 
 	/* --- Setting up TCP server ---  */
-		
-	
-	int tcp_sock;
-   /* Create a socket on the server side */
-   if ((tcp_sock = socket(PF_INET, SOCK_STREAM,0)) < 0) {
-      perror("ERROR: server-socket()\n");
-      exit(1);
-   } // end socket check
+   	/* Create a socket on the server side */
+   	if ((tcp_sock = socket(PF_INET, SOCK_STREAM,0)) < 0) {
+      		perror("ERROR: server-socket()\n");
+      		exit(1);
+   	} // end socket check
 
-   /* Set socket options */
-   if ((setsockopt(tcp_sock, SOL_SOCKET, SO_REUSEADDR, (char *)& opt, sizeof(int))) < 0) {
-      perror("ERROR: server-setsockopt()\n");
-      exit(1);
-   } //end socket options check
+   	/* Set socket options */
+   	if ((setsockopt(tcp_sock, SOL_SOCKET, SO_REUSEADDR, (char *)& opt, sizeof(int))) < 0) {
+      		perror("ERROR: server-setsockopt()\n");
+      		exit(1);
+   	} //end socket options check
 
 
 
-   /* Bind the created socket to the specified address */
-   if ((bind(tcp_sock,(struct sockaddr *)&sin, sizeof(sin))) < 0) {
-      perror("ERROR: server-bind()\n");
-      exit(1);
-   } //end bind check
+   	/* Bind the created socket to the specified address */
+   	if ((bind(tcp_sock,(struct sockaddr *)&sin, sizeof(sin))) < 0) {
+      		perror("ERROR: server-bind()\n");
+      		exit(1);
+   	} //end bind check
 
 	while(1) {
 		printf("Waiting for clien conection.\n");
-
                 if ((listen(tcp_sock, MAX_PENDING)) < 0) {
                         perror("ERROR: server-listen()\n");
                         exit(1);
@@ -140,53 +396,100 @@ int main (int argc, char * argv[]) {
 				user_exists = 1;
 			}
 		}
-
-
-	char request_password[150];
-	memset(request_password, '\0', sizeof(request_password));
-	snprintf(request_password, sizeof(request_password), "Please enter your password: ");
-	if (send(tcp_sock, request_password, sizeof(request_password), 0) == -1) {
-		perror("ERROR: server-send()");
-	}
-
-	char password[150];
-	if (recv(tcp_sock, password, sizeof(password), 0) == -1 ) {
-		perror("ERROR: server-recv()\n");
-	}
-	printf("PASSWORD: %s", password);
-	int password_match = -1;
-	if (user_exists != -1) {
-		int j;
-		for (j=0; j<100 && j<total_users; j++) {
-            		if ( strcmp (users[j].password,password)) {
-               			password_match = 1;
-            		}
-      		}
-	} else {
-		total_users++;
-		struct user u;
-		strcpy(u.name, username);
-		strcpy(u.password, password);
-		users[total_users] = u;
-		printf("HERE 4\n");
-		password_match = 1;
-	}
-	// Fail to authenticate user
-	size_t ACK = 1;
-	if (password_match == -1) {
-		ACK = 0;
-		printf("HERE 5\n");
-		printf("User Authentication Failed.\n");
-	} 
-	if (send(tcp_sock, &ACK, sizeof(size_t), 0) == -1 ) {
-		perror("ERROR: server-send()");
-		exit(1);
-	}
 	
-	
-		while(1) {
-			printf("Waiting for client operation \n");
-		}		
+		char request_password[150];
+		memset(request_password, '\0', sizeof(request_password));
+		snprintf(request_password, sizeof(request_password), "Please enter your password: ");
+		if (send(tcp_sock, request_password, sizeof(request_password), 0) == -1) {
+			perror("ERROR: server-send()");
+		}
+
+		char password[150];
+		if (recv(tcp_sock, password, sizeof(password), 0) == -1 ) {
+			perror("ERROR: server-recv()\n");
+		}
+		printf("PASSWORD: %s", password);
+		int password_match = -1;
+		if (user_exists != -1) {
+			int j;
+			for (j=0; j<100 && j<total_users; j++) {
+            			if ( strcmp (users[j].password,password)) {
+               				password_match = 1;
+            			}
+      			}
+		} else {
+			total_users++;
+			struct user u;
+			strcpy(u.name, username);
+			strcpy(u.password, password);
+			users[total_users] = u;
+			printf("HERE 4\n");
+			password_match = 1;
+		}
+		// Fail to authenticate user
+		size_t ACK = 1;
+		if (password_match == -1) {
+			ACK = 0;
+			printf("HERE 5\n");
+			printf("User Authentication Failed.\n");
+		} 
+		if (send(tcp_sock, &ACK, sizeof(size_t), 0) == -1 ) {
+			perror("ERROR: server-send()");
+			exit(1);
+		}
+		int connected = 1;
+		char protocol[10];
+		/* --------- WATING FOR USER OPERATION  --------- */
+		while(connected) {
+			printf("Waiting for operation from client.\n");
+			if (recvfrom(udp_sock, buf,strlen(buf),0,(struct sockaddr *)&sin, &addr_len) == -1) {
+               			perror("ERROR: client-recvfrom()\n");
+                		exit(1);
+        		} //end recvfrom check
+			printf("Received: %s\n", buf);
+
+
+                        if (strcmp(buf,"CRT")==0){
+				printf("Calling CREATE\n");
+                        	create(udp_sock, sin, username);
+                        }
+                        else if (strcmp(buf,"LIS")==0){
+                                //upload(sock);
+                        }
+                        else if (strcmp(buf,"MSG")==0){
+                                //list(sock);
+                        }
+                        else if (strcmp(buf,"DEL")==0){
+                                //makeDirectory(sock);
+                        }
+                        else if (strcmp(buf, "RDB")==0){
+                                //removeDirectory(sock);
+                        }
+                        else if (strcmp(buf, "EDT")==0){
+                                //changeDirectory(sock);
+                        }
+                        else if (strcmp(buf,"APN")==0){
+                                //delete(sock);
+                        }
+			else if (strcmp(buf,"DWN")==0) {
+				//dwonload
+			}
+			else if (strcmp(buf,"DST")==0) {
+				// destoy
+			}
+			else if (strcmp(buf,"SHT")==0) {
+				//shutdown
+			}
+                        else if (strcmp(buf,"XIT")==0){
+                                connected = 0;
+                                close(tcp_sock);
+                                printf("Client has ended session..\n");
+			}
+			memset(buf, '\0', sizeof(buf));
+			printf("Here 6\n");
+		}	
+		break;	
+
 	}
 
 }
